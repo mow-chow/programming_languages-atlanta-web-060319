f85f1c4aafe49897804195c9e7d2e4a1212943db
Catherine Cherubin <cat.cherubin@gmail.com>
2019-05-29 15:52:49 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,42 +1,14 @@
-languages = {
-  :ruby => {
-    :type => "interpreted",
-    :style => [:oo]
-  },
-  :javascript => {
-    :type => "interpreted",
-    :style => [:oo, :functional]
-  },
-  :python => {
-    :type => "interpreted",
-    :style => [:oo]
-  },
-  :java => {
-    :type => "compiled",
-    :style => [:oo]
-  },
-  :clojure => {
-    :type => "compiled",
-    :style => [:functional]
-  },
-  :erlang => {
-    :type => "compiled",
-    :style => [:functional]
-  },
-  :scala => {
-    :type => "compiled",
-    :style => [:functional]
-  }
-}
-
 def reformat_languages(languages)
-new_languages = {}
-languages.each do |type, language|
-  language.each do |name, attributes|
-  new_languages[name] ||= attributes
-  new_languages[name][:style]=[]
-  new_languages[name][:style]<<type
-end
-end
-new_languages
+  new_languages = {}
+  languages.each do |style, language|
+    language.each do |language_name, attribute|
+      if new_languages[language_name] == nil
+          new_languages[language_name] = attribute
+        end
+      new_languages[language_name][:style] = []
+      new_languages[language_name][:style] << style
+    end
+  end
+  new_languages[:javascript][:style] << :oo
+  new_languages
 end
